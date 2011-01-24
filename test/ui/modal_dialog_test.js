c3dbb39fe9290fb1bb2aeffbf6c1a7020e60b5ab
ysbaddaden <ysbaddaden@gmail.com>
2011-01-24 17:37:53 +0100

    More tests for UI.ModalDialog, and fixed the hide() method.

--- test/ui/modal_dialog_test.js
+++ test/ui/modal_dialog_test.js
@@ -20,61 +20,66 @@ new Unit.TestCase('UI.ModalDialogTest',
     this.assertUndefined(this.dialog.getTitle());
   },
 
-//  test_show_and_displayed: function()
-//  {
-//    this.dialog.initDialog();
-//    this.assertFalse(this.dialog.displayed());
-//    this.dialog.show();
-//    this.assertTrue(this.dialog.displayed());
-//    this.dialog.hide();
-//    this.assertFalse(this.dialog.displayed());
-//  },
+  test_show_and_displayed: function()
+  {
+    this.dialog.initModalDialog();
+    this.assertFalse(this.dialog.displayed());
+    this.assertFalse(this.dialog.overlay.displayed());
+    
+    this.dialog.show();
+    this.assertTrue(this.dialog.displayed());
+    this.assertTrue(this.dialog.overlay.displayed());
+    
+    this.dialog.hide();
+    this.assertFalse(this.dialog.displayed());
+    this.assertFalse(this.dialog.overlay.displayed());
+  },
 
-//  test_default_position: function()
-//  {
-//    this.dialog.initDialog();
-//    this.dialog.show();
-//    this.assertNotEqual('', this.dialog.container.style.top);
-//    this.assertNotEqual('', this.dialog.container.style.left);
-//  },
+  test_default_position: function()
+  {
+    this.dialog.initModalDialog();
+    this.dialog.show();
+    this.assertNotEqual('', this.dialog.container.style.top);
+    this.assertNotEqual('', this.dialog.container.style.left);
+  },
 
-//  test_top_position: function()
-//  {
-//    this.dialog.initDialog({position: 'top'});
-//    this.dialog.show();
-//    this.assertEqual('0px', this.dialog.container.style.top);
-//    this.assertNotEqual('', this.dialog.container.style.left);
-//    this.assertEqual('',    this.dialog.container.style.bottom);
-//    this.assertEqual('',    this.dialog.container.style.right);
-//  },
+  test_top_position: function()
+  {
+    this.dialog.initModalDialog({position: 'top'});
+    this.dialog.show();
+    this.assertEqual('0px', this.dialog.container.style.top);
+    this.assertNotEqual('', this.dialog.container.style.left);
+    this.assertEqual('',    this.dialog.container.style.bottom);
+    this.assertEqual('',    this.dialog.container.style.right);
+  },
 
-//  test_right_position: function()
-//  {
-//    this.dialog.initDialog({position: 'right'});
-//    this.dialog.show();
-//    this.assertNotEqual('', this.dialog.container.style.top);
-//    this.assertEqual('0px', this.dialog.container.style.right);
-//    this.assertEqual('',    this.dialog.container.style.bottom);
-//    this.assertEqual('',    this.dialog.container.style.left);
-//  },
+  test_right_position: function()
+  {
+    this.dialog.initModalDialog({position: 'right'});
+    this.dialog.show();
+    this.assertNotEqual('', this.dialog.container.style.top);
+    this.assertEqual('0px', this.dialog.container.style.right);
+    this.assertEqual('',    this.dialog.container.style.bottom);
+    this.assertEqual('',    this.dialog.container.style.left);
+  },
 
-//  test_bottom_position: function()
-//  {
-//    this.dialog.initDialog({position: 'bottom'});
-//    this.dialog.show();
-//    this.assertEqual('',    this.dialog.container.style.top);
-//    this.assertEqual('',    this.dialog.container.style.right);
-//    this.assertEqual('0px', this.dialog.container.style.bottom);
-//    this.assertNotEqual('', this.dialog.container.style.left);
-//  },
+  test_bottom_position: function()
+  {
+    this.dialog.initModalDialog({position: 'bottom'});
+    this.dialog.show();
+    this.assertEqual('',    this.dialog.container.style.top);
+    this.assertEqual('',    this.dialog.container.style.right);
+    this.assertEqual('0px', this.dialog.container.style.bottom);
+    this.assertNotEqual('', this.dialog.container.style.left);
+  },
 
-//  test_left_position: function()
-//  {
-//    this.dialog.initDialog({position: 'left'});
-//    this.dialog.show();
-//    this.assertNotEqual('', this.dialog.container.style.top);
-//    this.assertEqual('',    this.dialog.container.style.right);
-//    this.assertEqual('',    this.dialog.container.style.bottom);
-//    this.assertEqual('0px', this.dialog.container.style.left);
-//  }
+  test_left_position: function()
+  {
+    this.dialog.initModalDialog({position: 'left'});
+    this.dialog.show();
+    this.assertNotEqual('', this.dialog.container.style.top);
+    this.assertEqual('',    this.dialog.container.style.right);
+    this.assertEqual('',    this.dialog.container.style.bottom);
+    this.assertEqual('0px', this.dialog.container.style.left);
+  }
 });
