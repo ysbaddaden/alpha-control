868219b7d088b86c122553b5fd61b6c4e8994dec
ysbaddaden <ysbaddaden@gmail.com>
2011-01-27 11:06:33 +0100

    Fixed test of Function bind() with IE8.

--- test/support/function_test.js
+++ test/support/function_test.js
@@ -1,27 +1,35 @@
-//new Unit.TestCase('FunctionTest',
-//{
-//  test_bind: function()
-//  {
-//    var self = this, klass, instance, element, event;
-//    
-//    this.async();
-//    
-//    klass = function() { this.value = "titi"; }
-//    klass.prototype.onfocus = function(event)
-//    {
-//      var _self = this;
-//      self.sync(function() {
-//        self.assertEqual('titi', _self.value);
-//      });
-//    }
-//    
-//    instance = new klass();
-//    
-//    element = document.createElement('input');
-//    element.addEventListener('focus', instance.onfocus.bind(instance), false);
-//    
-//    event = document.createEvent('HTMLEvents');;
-//    event.initEvent('focus', true, true);
-//    element.dispatchEvent(event);
-//  }
-//});
+new Unit.TestCase('FunctionTest',
+{
+  setup: function()
+  {
+    this.element = document.createElement('input');
+    document.body.appendChild(this.element);
+  },
+
+  teardown: function() {
+    document.body.removeChild(this.element);
+  },
+
+  test_bind: function()
+  {
+    var self = this, klass, instance, element, event;
+    
+    this.async();
+    
+    klass = function() { this.value = "titi"; }
+    klass.prototype.oncustom = function(event)
+    {
+      var _self = this;
+      self.sync(function() {
+        self.assertEqual('titi', _self.value);
+      });
+    }
+    
+    instance = new klass();
+    this.element.addEventListener('custom', instance.oncustom.bind(instance), false);
+    
+    event = document.createEvent('HTMLEvents');
+    event.initEvent('custom', true, true);
+    this.element.dispatchEvent(event);
+  }
+});
