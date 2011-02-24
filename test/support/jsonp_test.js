27551b27a7701ba788b4bcbd3a1c623d7515d892
ysbadadden <ysbaddaden@gmail.com>
2011-02-24 21:58:16 +0100

    Fixed Color and JSONP.Request bugs with IE8.

--- test/support/jsonp_test.js
+++ test/support/jsonp_test.js
@@ -3,13 +3,10 @@ new Unit.TestCase('JSONP.RequestTest',
   test_request: function()
   {
     var self = this; this.async();
-    
-    window.showPost = function(post)
+    var showPost = function(post)
     {
-      self.sync(function()
-      {
+      self.sync(function() {
         self.assertEqual('2nd Post', post.post.title);
-        delete window.showPost;
       });
     }
     var req = new JSONP.Request();
@@ -20,13 +17,10 @@ new Unit.TestCase('JSONP.RequestTest',
   test_request_with_custom_callback_param: function()
   {
     var self = this; this.async();
-    
-    window.showPost = function(posts)
+    var showPost = function(posts)
     {
-      self.sync(function()
-      {
+      self.sync(function() {
         self.assertEqual(2, posts.length);
-        delete window.showPost;
       });
     }
     var req = new JSONP.Request();
@@ -38,20 +32,20 @@ new Unit.TestCase('JSONP.RequestTest',
   {
     var self = this; this.async();
     
-    window.showPost_1 = function(post)
+    var showPost_1 = function(post)
     {
       self.sync(function()
       {
         self.assertEqual('1st Post', post.post.title);
-        delete window.showPost_1;
+        window.showPost_1 = null;
       });
     };
-    window.showPost_2 = function(post) { delete window.showPost_2; };
-    window.showPost_3 = function(post) { delete window.showPost_3; };
+    var showPost_2 = function(post) { };
+    var showPost_3 = function(post) { };
     
     var req_1 = new JSONP.Request(); req_1.open('/posts/1.json',   showPost_3);
-    var req_2 = new JSONP.Request(); req_2.open('/posts/123.json', showPost_2);
-    var req_3 = new JSONP.Request(); req_3.open('/posts/0.json',   showPost_1);
+    var req_2 = new JSONP.Request(); req_2.open('/posts/0.json',   showPost_1);
+    var req_3 = new JSONP.Request(); req_3.open('/posts/123.json', showPost_2);
     
     req_1.send();
     req_2.send();
